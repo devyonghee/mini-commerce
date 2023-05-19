64d04e345dfdde06e003b54aea806eaa46ba6b7d
devyonghee <devyonghee@gmail.com>
2023-05-19 23:56:33 +0900

    feat(gateway): add gateway filter

---
+++ gateway/src/main/java/me/devyonghee/GatewayApplication.kt
@@ -0,0 +1,11 @@
+package me.devyonghee
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DiscoveryApplication
+
+fun main(args: Array<String>) {
+    runApplication<DiscoveryApplication>(*args)
+}
---
+++ gateway/src/main/java/me/devyonghee/configuration/FilterConfiguration.kt
@@ -0,0 +1,34 @@
+package me.devyonghee.configuration
+
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.cloud.gateway.route.builder.filters
+import org.springframework.cloud.gateway.route.builder.routes
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class FilterConfiguration {
+
+    @Bean
+    fun gatewayRoutes(builder: RouteLocatorBuilder): RouteLocator {
+        return builder.routes {
+            route {
+                path("/first-service/**")
+                uri("http://localhost:8081/")
+                filters {
+                    addRequestHeader("first-request", "first-request-header")
+                        .addResponseHeader("first-response", "first-response-header")
+                }
+            }
+            route {
+                path("/second-service/**")
+                uri("http://localhost:8082/")
+                filters {
+                    addRequestHeader("second-request", "second-request-header")
+                        .addResponseHeader("second-response", "second-response-header")
+                }
+            }
+        }
+    }
+}
