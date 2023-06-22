1e7d5caee90fb786533fc25b2bf9d281f02da9b0
devyonghee <devyonghee@gmail.com>
2023-06-22 19:51:19 +0900

    feat(jwt): add gateway AuthorizationHeaderFilter

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -2,4 +2,6 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 }
---
+++ gateway/src/main/kotlin/me/devyonghee/gateway/filter/AuthorizationHeaderFilter.kt
@@ -0,0 +1,59 @@
+package me.devyonghee.gateway.filter
+
+import io.jsonwebtoken.Jwts
+import me.devyonghee.gateway.configuration.JwtProperty
+import org.slf4j.LoggerFactory
+import org.springframework.cloud.gateway.filter.GatewayFilter
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.server.reactive.ServerHttpRequest
+import org.springframework.stereotype.Component
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
+
+@Component
+class AuthorizationHeaderFilter(
+    private val jwtProperty: JwtProperty,
+) : AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config>(Config::class.java) {
+
+    private val logger = LoggerFactory.getLogger(AuthorizationHeaderFilter::class.java)
+
+    override fun apply(config: Config): GatewayFilter {
+        return GatewayFilter { exchange, chain ->
+            val request: ServerHttpRequest = exchange.request
+
+            val authorization: Collection<String>? = request.headers[HttpHeaders.AUTHORIZATION]
+            if (authorization.isNullOrEmpty()) {
+                return@GatewayFilter onError(exchange, "No authorization header", HttpStatus.UNAUTHORIZED)
+            }
+
+            val jwt: String = authorization.first().replace("Bearer ", "")
+            if (isNotJwtValid(jwt)) {
+                return@GatewayFilter onError(exchange, "JWT token is not valid", HttpStatus.UNAUTHORIZED)
+            }
+            chain.filter(exchange)
+        }
+    }
+
+    private fun onError(exchange: ServerWebExchange, error: String, httpStatus: HttpStatus): Mono<Void> {
+        logger.error(error)
+        return exchange.response.apply { statusCode = httpStatus }.setComplete()
+    }
+
+    private fun isNotJwtValid(jwt: String): Boolean {
+        return try {
+            Jwts.parserBuilder()
+                .setSigningKey(jwtProperty.secret)
+                .build()
+                .parseClaimsJws(jwt)
+                .body
+                .subject
+                .isNullOrEmpty()
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+     class Config
+}
